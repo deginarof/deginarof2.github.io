f6de607c2c31425d3ca0ee1ef52729d26e2fc185
deginarof <deginapupt@hotmail.com>
2020-09-18 21:35:18 -0300

    validator

--- js/currentdate.js
+++ js/currentdate.js
@@ -2,6 +2,8 @@ const options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long',
-    year: 'numeric'
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric'
 };
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString("en-US", options);
