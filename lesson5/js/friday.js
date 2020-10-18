84613fb5acf3218a8e904b1ffeb3202a972dc441
deginarof <deginapupt@hotmail.com>
2020-10-18 02:15:26 -0300

    Pancake Message

---
+++ lesson5/js/friday.js
@@ -0,0 +1,9 @@
+function display(message) {
+    var day = date.getDay();
+    if (day > 5 || day < 5) {
+        return false;
+    } else {}
+    return true;
+}
+
+document.querySelector('.message').display = block;
