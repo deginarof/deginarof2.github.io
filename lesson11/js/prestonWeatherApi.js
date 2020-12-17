a6240aacc11e8459003f58a229400b34050fd3ec
deginarof <deginapupt@hotmail.com>
2020-12-17 11:54:44 -0300

    last update

--- lesson11/js/prestonWeatherApi.js
+++ lesson11/js/prestonWeatherApi.js
@@ -1,1 +1,9 @@
-const requestURL="https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=0a35ab49fbdb70d0abf8a387e577d3f4";fetch(requestURL).then(response=>response.json()).then(jsObject=>{console.log(jsObject),document.getElementById("current-temp").textContent=jsObject.main.temp,document.getElementById("description").textContent=jsObject.weather[0].description,document.getElementById("humidity").textContent=jsObject.main.humidity,document.getElementById("wind-speed").textContent=jsObject.wind.speed;const t=parseFloat(document.getElementById("current-temp").textContent),s=parseFloat(document.getElementById("wind-speed").textContent),output="Not Applicable";let w=35.74+.6215*t-35.75*Math.pow(s,.16)+.4275*t*Math.pow(s,.16);w=Math.round(w),document.getElementById("wind").innerHTML=t<=50&&s>3?w:output});
+const requestURL = "//api.openweathermap.org/data/2.5/weather?q=preston,us&appid=78b78816d97bf3ea58188b7f4b8332fb&units=imperial";
+fetch(requestURL).then(response => response.json()).then(jsObject => {
+    console.log(jsObject), document.getElementById("current-temp").textContent = jsObject.main.temp, console.log('temp', jsObject.main.temp), document.getElementById("description").textContent = jsObject.weather[0].description, document.getElementById("humidity").textContent = jsObject.main.humidity, document.getElementById("wind-speed").textContent = jsObject.wind.speed;
+    const t = parseFloat(document.getElementById("current-temp").textContent),
+        s = parseFloat(document.getElementById("wind-speed").textContent),
+        output = "Not Applicable";
+    let w = 35.74 + .6215 * t - 35.75 * Math.pow(s, .16) + .4275 * t * Math.pow(s, .16);
+    w = Math.round(w), document.getElementById("wind").innerHTML = t <= 50 && s > 3 ? w : output
+});
